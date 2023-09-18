a30be93874341b772984edc48925a73f6a8a0a28
Felicia Pranata <61774278+feliciapranata@users.noreply.github.com>
2023-09-18 18:36:26 +0200

    Update sql-join.sql

--- sql-bigquery/sql-join.sql
+++ sql-bigquery/sql-join.sql
@@ -29,9 +29,24 @@ SELECT orders.ord_no, orders.ord_date, orders.purch_amt, customer.cust_name AS C
 FROM orders INNER JOIN customer ON orders.customer_id = customer.customer_id INNER JOIN salesman ON orders.salesman_id = salesman.salesman_id
 
 Question 7
+SELECT * 
+FROM orders NATURAL JOIN customer NATURAL JOIN salesman
 
+Question 8
+SELECT customer.cust_name AS customer name, customer.cust_city AS customer city, customer.grade, salesman.name AS salesman, salesman.city AS salesman city
+FROM customer INNER JOIN salesman ON customer.salesman_id = salesman.salesman_id
+ORDER BY customer.customer_id
 
+Question 9
+SELECT customer.cust_name AS customer name, customer.cust_city AS customer city, customer.grade, salesman.name AS salesman, salesman.city AS salesman city
+FROM customer INNER JOIN salesman ON customer.salesman_id = salesman.salesman_id
+WHERE customer.grade < 300
+ORDER BY customer.customer_id  
 
+Question 10
+SELECT customer.cust_name, customer.city, orders.ord_no, orders.ord_date, orders.purch_amt
+FROM customer LEFT JOIN orders on customer.customer_id = orders.customer_id
+ORDER BY orders.ord_date
 
 
 
